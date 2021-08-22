ba885f5abe359257ef8d8701f32973da69a9a6c4
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-08-22 15:00:42 -0300

    feat: Criado componente temporizador

---
+++ components/Temporizador.tsx
@@ -0,0 +1,26 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+import styles from '../styles/Temporizador.module.css';
+
+interface TemporizadorProps {
+  duracao: number;
+  tempoEsgostado: () => void
+}
+
+export default function Temporizador(props: TemporizadorProps) {
+  return (
+    <div className={styles.Temporizador}>
+      <CountdownCircleTimer
+        duration={props.duracao} 
+        size={120}
+        isPlaying
+        onComplete={props.tempoEsgostado}
+        colors={[
+          ['#BCE596', 0.33],
+          ['#F7B801', 0.33],
+          ['#ED827A', 0.33]
+        ]}>
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+  )
+}
